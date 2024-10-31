5d6cc497fd7448631799a8fc3e3be541e5fe202c
Andrey <zxtorii@gmail.com>
2024-10-31 21:36:01 +0800

    loop

---
+++ Simple Java Examples/Conditional Statements/loop.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class loop {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while(1==1) {
+            System.out.println("I've been falling for 30 Minutes!!!!");
+
+        }
+    }
+}
